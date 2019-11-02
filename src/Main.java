99ddded838a68008995786932bf7a18581d0a665
yisusalanpng <alomeligcia@gmail.com>
2019-11-02 15:36:14 -0600

    Vamos a empezar

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import gui.VistaChat;
 
 public class Main {
     public static void main(String []args){
-        System.out.println("nisce");
+        System.out.println("nisces");
         VistaChat vc = new VistaChat();
         vc.verificacion();
 
